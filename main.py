2512901626615c4b43bdb03c68fb7bedc4fb3e09
vladskvortsov <vladskvortsov96@gmail.com>
2022-08-30 15:18:25 +0300

    template added

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+
+bot = telebot.TeleBot("5730700291:AAHHwCfg53dIqCtz5CCI46MgvIRyHwu1Cm4")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "How are you doing?")
+
+
+@bot.message_handler(content_types=['text'])
+def echo_l(message):
+    if message.text == 'Glory for Ukraine':
+        bot.send_message(message.chat.id, 'Glory for heroes!!!')
+
+
+bot.infinity_polling(none_stop=True)
+
